6fb611a2ed9e070b6038add9e49539189fd8c001
Mike <root@mikemamaril.com>
2021-04-27 00:18:59 +0800

    icons changes

--- packages/rice-uikit/src/widgets/Menu/components/CakePrice.tsx
+++ packages/rice-uikit/src/widgets/Menu/components/CakePrice.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { PancakeRoundIcon } from "../../../components/Svg";
 import Text from "../../../components/Text/Text";
 import Skeleton from "../../../components/Skeleton/Skeleton";
+import { useTheme } from "styled-components";
 
 interface Props {
   cakePriceUsd?: number;
@@ -22,9 +23,11 @@ const PriceLink = styled.a`
 `;
 
 const CakePrice: React.FC<Props> = ({ cakePriceUsd }) => {
+  const theme = useTheme();
+  
   return cakePriceUsd ? (
     <PriceLink href="https://pancakeswap.info/token/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82" target="_blank">
-      <PancakeRoundIcon width="24px" mr="8px" />
+      <PancakeRoundIcon isDark={theme.isDark} width="24px" mr="8px" />
       <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
     </PriceLink>
   ) : (
