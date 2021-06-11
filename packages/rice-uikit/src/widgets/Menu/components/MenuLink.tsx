fc3ff5c64332c48d6d012700a01065b360b5ddab
Boba Chef <bobagroup@protonmail.com>
2021-06-11 13:01:06 -0700

    ft: default external links open in new tab

--- packages/rice-uikit/src/widgets/Menu/components/MenuLink.tsx
+++ packages/rice-uikit/src/widgets/Menu/components/MenuLink.tsx
@@ -6,7 +6,8 @@ const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const Tag: any = isHttpLink ? "a" : NavLink;
-  const props = isHttpLink ? { href } : { to: href };
+  const target = '_blank'
+  const props = isHttpLink ? { href, target } : { to: href };
   return <Tag {...props} {...otherProps} />;
 };
 
